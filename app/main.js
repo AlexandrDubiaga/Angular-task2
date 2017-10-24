b673881a2d7a621db027648d167a6a32989f5ad0
student <student@GFL-1486.ad.geeksforless.net>
2017-10-24 20:27:59 +0300

    ledd

--- app/main.js
+++ app/main.js
@@ -1,23 +1,42 @@
-angular.module('app', [])
-var app = angular.module('app')
-app.controller('nameController', settingFunc);
-function settingFunc($scope)
-{
-    $scope.names = [
-        'Jani',
-        'Carl',
-        'Margareth',
-        'Hege',
-        'Joe',
-        'Gustav',
-        'Birgit',
-        'Mary',
-        'Kai'
-    ];
- this.names = "test this"
- var self = this
- this.names = []
- this.addName = function(name){
-     self.names.push(name);
- };
- }
+var app = angular.module('docsSimpleDirective', [])
+
+app.controller('Controller', function ($scope, nameStorage) {
+    console.log(nameStorage)
+  
+   
+  
+})
+    .factory('nameStorage', function () {
+        var product = []
+      return
+      {
+        getData: getArr()
+        {
+            return product
+        }
+        addData: addData(name)
+        {
+            product.push(name);
+        }
+
+      }
+});
+
+
+app.directive('myCustomer', function() {
+    return {
+      templateUrl: 'var.html',
+      scope:
+      {
+        param:'=info'
+      },
+      controller: 'myController',
+      link: function(scope, element, attrs, ctrl) {
+       
+      }
+    };
+   
+  });
+  app.controller('myCtrl', function($scope, $element, $attrs) {
+    this.name = 'myCtrl'
+  
